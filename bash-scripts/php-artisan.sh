8b1a8fa86e0e2ca2308577cbb58dd07cea8a09b1
Aris <empanta.aris@gmail.com>
2025-08-01 16:20:30 +0300

    renamed dockerhub repo

--- bash-scripts/php-artisan.sh
+++ bash-scripts/php-artisan.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd api && docker-compose -f ../compose.dev.yaml exec workspace php artisan "$@" && cd ../
+cd laravel-api && docker-compose -f ../compose.dev.yaml exec workspace php artisan "$@" && cd ../
